3c3e9a9ea6c45f82c35d5a3dbf3bdf1a7d7334db
Zachary Vernec <zachary.vernec@gmail.com>
2025-06-15 19:48:56 -0400

    Output to file instead of stdout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import itertools
 import logging
 import time
+from pathlib import Path
 
 import numpy as np
 from pytket.extensions.qiskit import qiskit_to_tk
@@ -175,24 +176,31 @@ def main():
     qpu_size = 8
     num_qubits_range = range(qpu_size*2, qpu_size*6+1, qpu_size)
 
-    for num_qubits in num_qubits_range:
+    for i,num_qubits in enumerate(num_qubits_range):
         circuit = QFT(num_qubits, do_swaps=False)
 
         num_partitions = num_qubits // qpu_size
         qpu_sizes = [qpu_size] * num_partitions # Equal sized QPUs
         depth = circuit.depth()
+        
+        output_string = ""
 
-        print(f"{qpu_size=}, {num_qubits=}, {num_partitions=}")
+        print(f"Testing {qpu_size=}, {num_qubits=}, {num_partitions=}")
+        output_string += f"{qpu_size=}, {num_qubits=}, {num_partitions=}\n"
 
         # Transpile the circuit to the basis gates
         basis_gates = ['u', 'cp']
         circuit = transpile(circuit, basis_gates=basis_gates) # TODO refactor
 
-        print(f'Number of qubits in circuit {circuit.num_qubits}')
+        output_string += f'Number of qubits in circuit {circuit.num_qubits}\n'
         best_score, time = test_PYTKET_AESD(circuit, qpu_sizes, num_partitions)
-        print(f"Min e-bit count: {best_score}")
-        print(f"Time taken for PYTKET_AESD: {time} seconds")
-        print()
+        output_string += f"Min e-bit count: {best_score}\n"
+        output_string += f"Time taken for PYTKET_AESD: {time} seconds\n"
+
+        filepath = Path(f'./results-data-pytket-aesd.txt')
+        mode = 'w' if i == 0 else 'a'
+        with filepath.open(mode, encoding='utf-8') as f:
+            print(output_string, file=f)
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.WARNING)
