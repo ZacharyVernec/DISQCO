da7b15796b7baf0429d178cf6a43e3601eb5df12
Zachary Vernec <zachary.vernec@gmail.com>
2025-06-16 15:08:19 -0400

    Produced many-partition, large circuit data for all methods Though, stopped early

--- plot_results_data.py
+++ plot_results_data.py
@@ -25,73 +25,72 @@ def plot_results():
         "PYTKET_AESD",
     ]
 
-    for num_partitions in [2, 4]:
-        folderpath = Path(f'./results_few_qpus/{num_partitions}_qpus/')
+    folderpath = Path(f'./results_massive_qft/')
 
-        # Initialize data storage
-        qubit_counts = {method:[] for method in method_names}
-        e_bit_counts = {method:[] for method in method_names}
-        times = {method:[] for method in method_names}
+    # Initialize data storage
+    qpu_counts = {method:[] for method in method_names}
+    e_bit_counts = {method:[] for method in method_names}
+    times = {method:[] for method in method_names}
 
-        for filename, method_name in zip(result_filenames, method_names):
+    for filename, method_name in zip(result_filenames, method_names):
 
-            filepath = folderpath / filename
+        filepath = folderpath / filename
 
-            with open(filepath, "r", encoding="utf-8") as f:
-                for line in f:
-                    if "num_qubits" in line:
-                        qubit_count = int(line.split(",")[1].split("=")[1])
-                        qubit_counts[method_name].append(qubit_count)
-                    elif "Min e-bit count" in line:
-                        e_bit_count = int(line.split(":")[1].strip())
-                        e_bit_counts[method_name].append(e_bit_count)
-                    elif "Time taken" in line:
-                        time = float(line.split(":")[1].split()[0].strip())
-                        times[method_name].append(time)
+        with open(filepath, "r", encoding="utf-8") as f:
+            for line in f:
+                if "num_partitions" in line:
+                    qpu_count = int(line.split(",")[2].split("=")[1].strip())
+                    qpu_counts[method_name].append(qpu_count)
+                elif "Min e-bit count" in line:
+                    e_bit_count = int(line.split(":")[1].strip())
+                    e_bit_counts[method_name].append(e_bit_count)
+                elif "Time taken" in line:
+                    time = float(line.split(":")[1].split()[0].strip())
+                    times[method_name].append(time)
 
-        for i in range(len(method_names)-1):
-            assert qubit_counts[method_names[i]] == qubit_counts[method_names[i+1]], f"Qubit counts do not match across methods {method_names[i]} and {method_names[i+1]}"
-        qubit_counts = qubit_counts[method_names[0]]
-        x = np.arange(len(qubit_counts))
+    for i in range(len(method_names)-1):
+        assert qpu_counts[method_names[i]] == qpu_counts[method_names[i+1]], f"Qubit counts do not match across methods {method_names[i]} and {method_names[i+1]}"
+    qpu_counts = qpu_counts[method_names[0]]
+    x = np.arange(len(qpu_counts))
 
-        # Plotting
-        width = 1 / (len(method_names)+1)  # the width of the bars
-        figsize=(width*len(qubit_counts)*20, 5)
+    # Plotting
+    width = 1 / (len(method_names)+1)  # the width of the bars
+    figsize=(width*len(qpu_counts)*20, 5)
 
-        # Plot e-bit count vs num_qubits
-        plt.figure(figsize=figsize)
-        for i, (method, e_bit_count) in enumerate(e_bit_counts.items()):
-            offset = width * i
-            rects = plt.bar(x + offset, e_bit_count, width, label=method)
-            plt.bar_label(rects, padding=3)
-        plt.xlabel("Number of Qubits")
-        plt.ylabel("Min E-bit Count")
-        plt.title(f"E-bit Count vs Number of Qubits ({num_partitions} QPUs)")
-        plt.xticks(x, qubit_counts)
-        plt.legend(loc='upper left', ncols=len(method_names))
-        plt.legend()
-        plt.savefig(folderpath / "bar_chart_e_bit_count_vs_num_qubits.png")
-        
-        # Plot time vs num_qubits
-        plt.figure(figsize=figsize)
-        def formatter(f: float):
-            if f == 0:
-                return "0"
-            elif f < 1:
-                return "~0"
-            else:
-                return str(int(f))
-        for i, (method, time) in enumerate(times.items()):
-            offset = width * i
-            rects = plt.bar(x + offset, time, width, label=method)
-            plt.bar_label(rects, fmt=formatter, padding=3)
-        plt.xlabel("Number of Qubits")
-        plt.ylabel("Time (seconds)")
-        plt.title(f"Time vs Number of Qubits ({num_partitions} QPUs)")
-        plt.xticks(x, qubit_counts)
-        plt.legend(loc='upper left', ncols=len(method_names))
-        plt.legend()
-        plt.savefig(folderpath / "bar_chart_time_vs_num_qubits.png")
+    # Plot e-bit count vs num_qubits
+    plt.figure(figsize=figsize)
+    for i, (method, e_bit_count) in enumerate(e_bit_counts.items()):
+        offset = width * i
+        rects = plt.bar(x + offset, e_bit_count, width, label=method)
+        plt.bar_label(rects, padding=3)
+    plt.xlabel("Number of QPUs")
+    plt.ylabel("Min E-bit Count")
+    plt.title("E-bit Count vs Number of QPUs (for 256 qubits)")
+    plt.xticks(x, qpu_counts)
+    plt.legend(loc='upper left', ncols=len(method_names))
+    plt.legend()
+    plt.savefig(folderpath / "bar_chart_e_bit_count_vs_num_qpus.png")
+    
+    # Plot time vs num_qubits
+    plt.figure(figsize=figsize)
+    def formatter(f: float):
+        if f == 0:
+            return "0"
+        elif f < 1:
+            return "~0"
+        else:
+            return str(int(f))
+    for i, (method, time) in enumerate(times.items()):
+        offset = width * i
+        rects = plt.bar(x + offset, time, width, label=method)
+        plt.bar_label(rects, fmt=formatter, padding=3)
+    plt.xlabel("Number of QPUs")
+    plt.ylabel("Time (seconds)")
+    plt.title("Time vs Number of QPUs (for 256 qubits)")
+    plt.xticks(x, qpu_counts)
+    plt.legend(loc='upper left', ncols=len(method_names))
+    plt.legend()
+    plt.savefig(folderpath / "bar_chart_time_vs_num_qpus.png")
 
 if __name__ == "__main__":
     plot_results()
