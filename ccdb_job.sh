7344a06ba2213ce0691bf64e3872264c95cf3b60
Zachary Vernec <zachary.vernec@gmail.com>
2025-06-21 19:20:58 -0400

    Moved to appcontainer-based environment

--- ccdb_job.sh
+++ ccdb_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=def-jacobsen  # The group's allocation
 #SBATCH -N 1                    # Number of nodes (computers) (usually 1)
-#SBATCH -n 32                    # Number of CPU cores (processors)
+#SBATCH -n 16                    # Number of CPU cores (processors)
 #SBATCH --mem-per-cpu=512M      # Memory per CPU core (e.g., 512M, 1G, 4G)
                                 # (Don't use this specific option on Niagara)
 #SBATCH --time=00-00:20:00      # Max runtime (Days-HH:MM:SS) - JOB KILLED AFTER THIS!
 #SBATCH --job-name=SmallQFT # Descriptive job name
-#SBATCH --output=/project/def-jacobsen/zachvern/logs/%x-%j.out  # Standard output file (%x=jobname, %j=jobid)
+#SBATCH --output=/home/zachvern/logs/%x-%j.out  # Standard output file (%x=jobname, %j=jobid)
                                                       # Ensure '/path/to/your/project/logs' exists on HPC!
-#SBATCH --error=/project/def-jacobsen/zachvern/logs/%x-%j.err   # Standard error file
+#SBATCH --error=/home/zachvern/logs/%x-%j.err   # Standard error file
                                                       # Ensure '/path/to/your/project/logs' exists on HPC!
 #SBATCH --mail-user=zachary.vernec@mail.utoronto.ca # Optional: Email for job status
 #SBATCH --mail-type=FAIL,INVALID_DEPEND,REQUEUE,STAGE_OUT #,END # Email events
@@ -17,35 +17,14 @@ echo "Job started on $(hostname) at: $(date)"
 echo "Job ID: $SLURM_JOB_ID"
 echo "My working directory on HPC is: $(pwd)" # This will be your submission directory
 
-
 echo "--- Setting up software ---"
-module load StdEnv/2020
-module load python/3.11
-module load kahypar
-module load scipy-stack/2025a
+module load apptainer
 
-# Create a temporary, private workspace for this job on the HPC
-# $SLURM_TMPDIR is a special fast, temporary directory for your job on the HPC
-# Using it is often faster for installing packages and running code
-virtualenv --no-download "$SLURM_TMPDIR/env"
-source "$SLURM_TMPDIR/env/bin/activate" # Activate virtual environment
-
-echo "--- Installing Python packages ---"
-# Using --no-index tells pip not to look online, good if pre-downloaded wheels are available
-# See: https://docs.alliancecan.ca/wiki/Python#Installing_packages
-pip install --no-index --upgrade pip
-pip install --no-index -r "/project/def-jacobsen/zachvern/requirements.txt"
-
-echo "--- Running my Python script ---"
-# Replace with your actual command. Ensure paths to script/data are correct on HPC.
-python "/project/def-jacobsen/zachvern/main.py" #--input /path/to/data --output "$SLURM_TMPDIR/results.csv"
-
-SCRIPT_EXIT_CODE=$? # Save script's exit code
-echo "--- Python script finished with exit code $SCRIPT_EXIT_CODE ---"
+apptainer run -C -W ${SLURM_TMPDIR} image.sif
 
 # If your script created important files in $SLURM_TMPDIR, copy them back to persistent storage
 echo "--- Copying data to home (/project/def-jacobsen/zachvern/) ---"
-cp -v "$SLURM_TMPDIR/results-data-{gcp-s,gcp-e,fgp-roee,mlfm-r,zv-thy,pytket-de,pytket-aesd}.txt" "/project/def-jacobsen/zachvern"
+cp -v "$SLURM_TMPDIR/tmp/results-data-{gcp-s,gcp-e,fgp-roee,mlfm-r,zv-thy,pytket-de,pytket-aesd}.txt" "/home/zachvern/"
 
 echo "--- Job finished at: $(date) ---"
 exit $SCRIPT_EXIT_CODE # Important: Exit with your script's exit code
--- ccdb_login.sh
+++ ccdb_login.sh
@@ -1,26 +1,3 @@
-
-module spider scipy-stack/2020a #to get scipy version numbers
-
-module load StdEnv/2020
-module load python/3.11
-module load gcc/9.3.0
-module load kahypar
-module load scipy-stack/2023b
-ENVDIR=/tmp/$RANDOM
-virtualenv --no-download $ENVDIR
-source $ENVDIR/bin/activate
-pip install --no-index --upgrade pip
-pip install --no-index kahypar numpy qiskit qiskit-aer networkx matplotlib pylatexenc pytket pytket-qiskit hypernetx celluloid igraph decorator pygraphviz importlib_resources pyzx pytket-pyzx
-pip download --no-deps git+https://github.com/CQCL/pytket-dqc.git
-pip install --no-index path/to/pytket-dqc
-pip download --no-deps git+https://github.com/felix-burt/DISQCO.git
-pip install --no-index path/to/DISQCO
-
-pip freeze --local > /project/def-jacobsen/zachvern/requirements.txt
-deactivate
-rm -rf $ENVDIR
-
-
-# copy over files: job should be in home, so are python scripts
-# ccdb_job.sh
-# main.py only, but modify n_qubits
+# copy over files into home
+# - DISQCO/ccdb_job.sh
+# - image.sif created by  APPTAINER_BIND=' ' apptainer build image.sif image.def 
